72ee444fe18317b932164e0544c9cea39637c067
Guillermo Tala <guille86598@gmail.com>
2024-10-01 10:53:31 -0300

    fix: test

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import React, {useState} from 'react';
 import './App.css';
 import {fromB64} from "@mysten/sui/utils";
 import {Ed25519Keypair} from "@mysten/sui/keypairs/ed25519";
+import {Transaction} from "@mysten/sui/transactions";
+import {getFullnodeUrl, SuiClient} from "@mysten/sui/client";
+import {Signer} from "@mysten/sui/cryptography"
+import {SUI_CLOCK_OBJECT_ID} from "@mysten/sui/utils";
+
 
 
 //@ts-ignore
 import {execHaloCmdWeb} from "@arx-research/libhalo/api/web.js";
 
+
+export const getSignatureAsUint8Array = async (
+    r: string,
+    s: string,
+    v: number
+) => {
+  // Convert r, s values to Uint8Array
+  const rBytes = Uint8Array.from(Buffer.from(r, "hex"));
+  const sBytes = Uint8Array.from(Buffer.from(s, "hex"));
+  const vByte = Uint8Array.from([v]);
+  // @ts-ignore
+  const signature = new Uint8Array([...rBytes, ...sBytes, ...vByte]);
+
+  return signature;
+};
+
+export const mySuiClient = new SuiClient({url: getFullnodeUrl("testnet")});
 const hexToUint8Array = (hex: any) => {
   let bytes = [];
   for (let i = 0; i < hex.length; i += 2) {
@@ -70,14 +92,65 @@ const getMessageToSign = async (
 
   return msgToSignBytes;
 };
+
+const packageID = '0x30da050ef8a0959023b2d5d25ff7a67c036745253c923d5e8361af2b717f6aa5'
+const adminCap = "0xe6016206b1177cb44c1467a4aa43dc0e8838b8030dc03ca501c281895ced8d58"
+const pbtArchive = "0x57e282bb30b2410983d6c16d6dbdeb661f203e0cd2a480a57aedfbf81f551d78"
+
+
+
+export const pbt_mint = async (
+    chipSignature: Uint8Array,
+    chipPK: Uint8Array,
+    keyPair: Signer,
+) => {
+  // Get the latest DRAND beacon value
+  const drandBeaconValue = await getLatestDRANDBeaconValue();
+
+  // Get the bytes from the DRAND signature.
+  let drandSignatureBytes: Uint8Array = new Uint8Array(
+      Buffer.from(drandBeaconValue.signature, "hex"),
+  );
+
+  // Get the bytes for the previous DRAND signature.
+  let drandPreviousSignatureBytes: Uint8Array = new Uint8Array(
+      Buffer.from(drandBeaconValue.previous_signature, "hex"),
+  );
+
+  const tx = new Transaction();
+  tx.setGasBudget(100000000);
+  tx.moveCall({
+    target: `${packageID}::pbt::pbt`,
+    arguments: [
+      tx.pure.vector("u8", Array.from(chipSignature)),
+      tx.pure.vector("u8", Array.from(chipPK)),
+      tx.pure.vector("u8", Array.from(drandSignatureBytes)),
+      tx.pure.vector("u8", Array.from(drandPreviousSignatureBytes)),
+      tx.pure.u64(drandBeaconValue.round),
+      tx.object(pbtArchive),
+      tx.object(SUI_CLOCK_OBJECT_ID),
+    ],
+  });
+
+  const response = await mySuiClient.signAndExecuteTransaction({
+    signer: keyPair,
+    transaction: tx,
+    requestType: "WaitForLocalExecution",
+    options: {
+      showEffects: true,
+      showEvents: true,
+      showObjectChanges: true,
+    },
+  });
+  return response;
+};
+
 function App() {
   const [statusText, setStatusText] = useState('Click on the button');
   const [dataAdddess, setDataAddress] = useState(new Uint8Array([]));
   const [dataDrand, setDataDrand] = useState(new Uint8Array([]));
 
   async function btnClick() {
-
-
     let userPrivateKeyArray = new Array<number>();
     userPrivateKeyArray = Array.from(fromB64("AHvhK7acqcgXdkSR+gE5jzTFXjvWvH7hdYOesk/MR+07"));
     userPrivateKeyArray.shift(); // remove the first byte
@@ -115,6 +188,23 @@ function App() {
           }
         }
       });
+
+      let signature = await getSignatureAsUint8Array(
+          res.signature.raw.r,
+          res.signature.raw.s,
+          res.signature.raw.v
+      );
+
+      console.log("signature", signature)
+
+      const mintRes = await pbt_mint(
+          signature,
+          res.publicKey,
+          userKeypair,
+      );
+
+      console.log("mintRes", mintRes.digest)
+
       // the command has succeeded, display the result to the user
       setStatusText(JSON.stringify(res, null, 4));
     } catch (e) {

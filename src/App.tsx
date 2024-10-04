c47f3cd884c74533f1bf9a71046b24790de3f88d
Guillermo Tala <guille86598@gmail.com>
2024-10-04 11:24:50 -0300

    feat: refactor

--- src/App.tsx
+++ src/App.tsx
@@ -14,66 +14,7 @@ import { sha256 } from "@noble/hashes/sha256";
 //@ts-ignore
 import {execHaloCmdWeb} from "@arx-research/libhalo/api/web.js";
 
-function bytesToHex(bytes: number[]): string {
-  return '0x' + bytes.map(byte => byte.toString(16).padStart(2, '0')).join('');
-}
-
-function compressPublicKeyToUint8Array(hex: string): Uint8Array {
-  // Convert hex string to byte array
-  const uncompressedKey = hexToBytes(hex);
-
-  // Ensure the key starts with 0x04 (uncompressed format)
-  if (uncompressedKey[0] !== 0x04) {
-    throw new Error("Not a valid uncompressed public key");
-  }
-
-  // Extract x and y coordinates
-  const xCoord = uncompressedKey.slice(1, 33);  // First 32 bytes (after 0x04) is x-coordinate
-  const yCoord = uncompressedKey.slice(33);     // Next 32 bytes is y-coordinate
-
-  // Determine if y-coordinate is even or odd
-  const prefix = (yCoord[yCoord.length - 1] % 2 === 0) ? 0x02 : 0x03;
-
-  // Construct the compressed public key
-  const compressedKey = new Uint8Array(33);
-  compressedKey[0] = prefix; // Prefix (0x02 or 0x03)
-  compressedKey.set(xCoord, 1); // Set x-coordinate
-
-  return compressedKey;
-}
-
-// Helper function: Converts a hex string to a byte array (Uint8Array)
-function hexToBytes(hex: string): Uint8Array {
-  const bytes = new Uint8Array(hex.length / 2);
-  for (let i = 0; i < hex.length; i += 2) {
-    bytes[i / 2] = parseInt(hex.substring(i, i + 2), 16);
-  }
-  return bytes;
-}
-
-
 export const mySuiClient = new SuiClient({url: getFullnodeUrl("testnet")});
-const hexToUint8Array = (hex: any) => {
-  let bytes = [];
-  for (let i = 0; i < hex.length; i += 2) {
-    bytes.push(parseInt(hex.substr(i, 2), 16));
-  }
-  return new Uint8Array(bytes);
-};
-
-const uncompressToUint8 = (uncompressedPubKey: string)=> {
-
-  const xCoord = uncompressedPubKey.slice(0, 64);  // First 64 hex characters (32 bytes)
-  const yCoord = uncompressedPubKey.slice(64);     // Remaining 64 hex characters (32 bytes)
-  const yBigInt = BigInt("0x" + yCoord);
-  // @ts-ignore
-  const isEven = (yBigInt % 2n === 0n);
-  const prefix = isEven ? "02" : "03";
-  const compressedPubKey = prefix + xCoord;
-
-  console.log("Compressed Public Key:", compressedPubKey);
-  return  hexToUint8Array(compressedPubKey);
-}
 
 const getLatestDRANDBeaconValue = async () => {
   const response = await fetch("https://api.drand.sh/public/latest");
@@ -87,39 +28,8 @@ const getLatestDRANDBeaconValue = async () => {
   return data;
 };
 
-const getMessageToSign = async (
-    address: string,
-    extra_byte: boolean,
-) => {
-  console.log("address", address);
-  let msgToSign: Array<Uint8Array> = [];
-  let addressBytes: Uint8Array = new Uint8Array(
-      Buffer.from(address.slice(2), "hex"),
-  );
-  msgToSign.push(addressBytes);
-
-  const drandData = await getLatestDRANDBeaconValue();
-  console.log("drandData.signature", drandData.signature)
-  let drandSignatureBytes: Uint8Array = new Uint8Array(
-      Buffer.from(drandData.signature, "hex"),
-  );
-  msgToSign.push(drandSignatureBytes);
-  let msgToSignBytes: Uint8Array = new Uint8Array();
-  msgToSign.forEach((msg) => {
-    //@ts-ignore
-    msgToSignBytes = Uint8Array.from([...msgToSignBytes, ...msg]);
-  });
-  if (extra_byte) {
-    //@ts-ignore
-    msgToSignBytes = new Uint8Array([...msgToSignBytes, 0]);
-  }
-
-  return msgToSignBytes;
-};
-
-const packageID = '0x30da050ef8a0959023b2d5d25ff7a67c036745253c923d5e8361af2b717f6aa5'
-const adminCap = "0xe6016206b1177cb44c1467a4aa43dc0e8838b8030dc03ca501c281895ced8d58"
-const pbtArchive = "0x57e282bb30b2410983d6c16d6dbdeb661f203e0cd2a480a57aedfbf81f551d78"
+const PBT_PACKAGE_ID = '0x30da050ef8a0959023b2d5d25ff7a67c036745253c923d5e8361af2b717f6aa5'
+const ARCHIVE_OBJECT_ID = "0x57e282bb30b2410983d6c16d6dbdeb661f203e0cd2a480a57aedfbf81f551d78"
 
 export const readTheCorrectPublicKey = async (
     publicKeyDigest: string,
@@ -142,8 +52,6 @@ export const readTheCorrectPublicKey = async (
   return [pkey_final, signature_final];
 };
 
-
-
 export const pbt_mint = async (
     chipSignature: Uint8Array,
     chipPK: Uint8Array,
@@ -165,14 +73,14 @@ export const pbt_mint = async (
   const tx = new Transaction();
   tx.setGasBudget(100000000);
   tx.moveCall({
-    target: `${packageID}::pbt::mint`,
+    target: `${PBT_PACKAGE_ID}::pbt::mint`,
     arguments: [
       tx.pure.vector("u8", Array.from(chipSignature)),
       tx.pure.vector("u8", Array.from(chipPK)),
       tx.pure.vector("u8", Array.from(drandSignatureBytes)),
       tx.pure.vector("u8", Array.from(drandPreviousSignatureBytes)),
       tx.pure.u64(drandBeaconValue.round),
-      tx.object(pbtArchive),
+      tx.object(ARCHIVE_OBJECT_ID),
       tx.object(SUI_CLOCK_OBJECT_ID),
     ],
   });
@@ -193,7 +101,8 @@ export const pbt_mint = async (
 function uint8array2hex(uint8array: Uint8Array): string {
   return Buffer.from(uint8array).toString("hex");
 }
-const createMsgDigest =  async (address: string, timestamp: number) => {
+const buildMessageToSign =  async (address: string) => {
+
   const drandData = await getLatestDRANDBeaconValue();
   const addr_bytes = bcs.Address.serialize(address).toBytes()
   const ts_bytes = bcs.vector(bcs.u8()).serialize(Array.from(new Uint8Array(Buffer.from(drandData.signature, "hex")))).toBytes()
@@ -203,6 +112,17 @@ const createMsgDigest =  async (address: string, timestamp: number) => {
   return uint8array2hex(sha256(msgToDigest));
 };
 
+function getUserKeyPairData() {
+  let userPrivateKeyArray = new Array<number>();
+  userPrivateKeyArray = Array.from(fromB64("AHvhK7acqcgXdkSR+gE5jzTFXjvWvH7hdYOesk/MR+07"));
+  userPrivateKeyArray.shift(); // remove the first byte
+  const userKeypair = Ed25519Keypair.fromSecretKey(
+      Uint8Array.from(userPrivateKeyArray),
+  );
+  const userAddress = userKeypair.getPublicKey().toSuiAddress();
+  console.log("userAddress", userAddress)
+  return {userKeypair, userAddress};
+}
 
 function App() {
   const [statusText, setStatusText] = useState('Click on the button');
@@ -212,35 +132,20 @@ function App() {
   const [stateSignature, setStateSignature] = useState(new Uint8Array([]));
 
 
+  async function btnReadChipAndMintPBT() {
 
-  async function btnClick() {
-    let userPrivateKeyArray = new Array<number>();
-    userPrivateKeyArray = Array.from(fromB64("AHvhK7acqcgXdkSR+gE5jzTFXjvWvH7hdYOesk/MR+07"));
-    userPrivateKeyArray.shift(); // remove the first byte
-
-    const userKeypair = Ed25519Keypair.fromSecretKey(
-        Uint8Array.from(userPrivateKeyArray),
-    );
-    const userAddress = userKeypair.getPublicKey().toSuiAddress();
-
-    console.log("userAddress", userAddress)
-
-
-    const messageToSign  = await getMessageToSign(userAddress, false )
-
-    const message =  await createMsgDigest(userAddress, Date.now())
-
-    let command = {
-      name: "sign",
-      keyNo: 1,
-      digest: message
-    };
+    //1) create signer KeyPair
+    const {userKeypair, userAddress} = getUserKeyPairData();
 
     let res;
 
     try {
-      // --- request NFC command execution ---
-      res = await execHaloCmdWeb(command, {
+      // 2) Get signed message
+      res = await execHaloCmdWeb({
+        name: "sign",
+        keyNo: 1,
+        digest: await buildMessageToSign(userAddress)
+      }, {
         statusCallback: (cause: any) => {
           if (cause === "init") {
             setStatusText("Please tap the tag to the back of your smartphone and hold it...");
@@ -254,19 +159,15 @@ function App() {
         }
       });
 
-      let signature = res.signature.raw.r + res.signature.raw.s
-     setStateSignature(hexToBytes(signature))
-
-      const compressedKey = compressPublicKeyToUint8Array(res.publicKey);
-      setPublickKey(compressedKey)
-
+    // 3) format signed message and pk
       let [pkey_final, signature_final] = await readTheCorrectPublicKey(
           res.publicKey,
           res.signature.raw.r + res.signature.raw.s
       );
 
-      console.log(pkey_final)
+      console.log(pkey_final, signature_final)
 
+      // 4) Call Mint PBT function
       const mintRes = await pbt_mint(
           signature_final,
           pkey_final,
@@ -288,7 +189,7 @@ function App() {
             <pre style={{fontSize: 12, textAlign: "left", whiteSpace: "pre-wrap", wordWrap: "break-word"}}>
                 {statusText}
             </pre>
-        <button onClick={() => btnClick()}>Sign message 010203 using key #1</button>
+        <button onClick={() => btnReadChipAndMintPBT()}>Sign message 010203 using key #1</button>
         <p>{dataAdddess}</p>
         <p>{dataDrand}</p>
         <>{publickKey}</>

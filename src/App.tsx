20b97a8980fff6a51fef4e9b7239275052e40b54
Guillermo Tala <guille86598@gmail.com>
2024-10-16 23:31:57 -0300

    feat: signature

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,26 @@ import {SUI_CLOCK_OBJECT_ID} from "@mysten/sui/utils";
 //@ts-ignore
 import {execHaloCmdWeb} from "@arx-research/libhalo/api/web.js";
 import { sha256 } from "@noble/hashes/sha256";
+import { bcs } from "@mysten/sui/bcs";
 
+const mapScannedMessage = new Map<string,string>([
+    ["init", "Please tap the tag to the back of your smartphone and hold it..."],
+    ["retry", "Something went wrong, please try to tap the tag again..."],
+    ["scanned", "Tag scanned successfully, post-processing the result..."]
+    ])
 
 const mySuiClient = new SuiClient({url: getFullnodeUrl("testnet")});
 const PBT_PACKAGE_ID = '0xcdb598fa18496f01295b53c711f19b756de8f813368de6054c7f55ae061f603e'//'0x30da050ef8a0959023b2d5d25ff7a67c036745253c923d5e8361af2b717f6aa5'
-const message = 'bcf83051a4d206c6e43d7eaa4c75429737ac0d5ee08ee68430443bd815e6ac05'
+const message = '0x1e41bacaef7d6a7c1c82b1077d9833bda6d9427012ab4825bb2d129823432908'
+
 function App() {
+
   const [statusText, setStatusText] = useState('Click on the button');
 
   const haloOptions = {
-    statusCallback: (cause: any) => {
-      if (cause === "init") {
-        setStatusText("Please tap the tag to the back of your smartphone and hold it...");
-      } else if (cause === "retry") {
-        setStatusText("Something went wrong, please try to tap the tag again...");
-      } else if (cause === "scanned") {
-        setStatusText("Tag scanned successfully, post-processing the result...");
-      } else {
-        setStatusText(cause);
-      }
+    statusCallback: (cause: string) => {
+      const status = mapScannedMessage.get(cause)
+      setStatusText(status ? status : cause);
     }
   }
 
@@ -36,12 +37,13 @@ function App() {
     const {userKeypair, userAddress} = getUserKeyPairData();
     let scannedResult;
 
+    const digestMessage = createMsgDigestV2(message)
 
     try {
       scannedResult = await execHaloCmdWeb({
         name: "sign",
         keyNo: 1,
-        digest: createMsgDigest(message)
+        digest: digestMessage
       }, haloOptions);
 
       let [pkey_final, signature_final] = await readTheCorrectPublicKey(
@@ -49,18 +51,14 @@ function App() {
           scannedResult.signature.raw.r + scannedResult.signature.raw.s
       );
 
-
       const tx = new Transaction();
       tx.setGasBudget(2000000);
       tx.moveCall({
         target:`${PBT_PACKAGE_ID}::signature::verify_signature_v2`,
         arguments: [
-         // tx.pure.vector("u8", Array.from(Buffer.from("6a4727823b14b210a22d7a41da2b484e7aab5d89af3a4b8e99cda3feed5dd7915e212be7ab47e40b843387e61b95dbfb26c6f8c5e2301d78a8d9172ad55a52aa", "hex"))),
-         // tx.pure.vector("u8", Array.from(Buffer.from("029bef8d556d80e43ae7e0becb3a7e6838b95defe45896ed6075bb9035d06c9964", "hex"))),
-
           tx.pure.vector("u8", signature_final),
           tx.pure.vector("u8", pkey_final),
-          tx.pure.vector("u8", Array.from(Buffer.from(message, "hex"))),
+          tx.pure.vector("u8", Array.from(Buffer.from(digestMessage, "hex"))),
         ],
       });
       const response = await mySuiClient.signAndExecuteTransaction({
@@ -103,7 +101,7 @@ function getUserKeyPairData() {
   return {userKeypair, userAddress};
 }
 
-const readTheCorrectPublicKey = async (
+export const readTheCorrectPublicKey = async (
     publicKeyDigest: string,
     signatureDigest: string
 ) => {
@@ -120,13 +118,15 @@ const readTheCorrectPublicKey = async (
 
   const pkey_final = Uint8Array.from(pkey);
   const signature_final = Uint8Array.from(Buffer.from(signatureDigest, "hex"));
+
   return [pkey_final, signature_final];
 };
 
-export const createMsgDigest =  (message: string) => {
-  const message_bytes =  Array.from(Buffer.from(message, "hex"))
-  const msgToDigest = new Uint8Array(message_bytes.length);
-  msgToDigest.set(message_bytes);
+export const createMsgDigestV2 =  (address: string) => {
+  console.log("address", address);
+  const addr_bytes = bcs.Address.serialize(address).toBytes();
+  const msgToDigest = new Uint8Array(addr_bytes.length);
+  msgToDigest.set(addr_bytes);
   const msgDigestHex = uint8array2hex(sha256(msgToDigest));
 
   return msgDigestHex

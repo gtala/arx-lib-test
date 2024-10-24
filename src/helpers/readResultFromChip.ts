dad55d7009464d6c720ecd63854f838a3c5afeab
Guillermo Tala <guille86598@gmail.com>
2024-10-24 08:37:52 -0300

    rotating lines

--- src/helpers/readResultFromChip.ts
+++ src/helpers/readResultFromChip.ts
@@ -7,9 +7,11 @@ export const readTheCorrectPublicKey =  (
   pkey = pkey.slice(0, 32);
   //depends on last byte, unshift 2 or 3 to sub with last number to be even
   if (pkey[pkey.length - 1] % 2 == 0) {
-    pkey.unshift(2);
-  } else {
+   // pkey.unshift(2); //original way
     pkey.unshift(3);
+  } else {
+    //pkey.unshift(3); //original way
+    pkey.unshift(2);
   }
   const pkey_final = Uint8Array.from(pkey);
   const signature_final = Uint8Array.from(Buffer.from(signatureDigest, "hex"));

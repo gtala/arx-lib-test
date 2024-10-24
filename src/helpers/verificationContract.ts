6b714dabf328b96a30d308223e8f09ccb2e10a5d
Guillermo Tala <guille86598@gmail.com>
2024-10-24 08:06:39 -0300

    fix

--- src/helpers/verificationContract.ts
+++ src/helpers/verificationContract.ts
@@ -1,11 +1,17 @@
 
-import { fromB64 } from '@mysten/bcs'
-import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519'
-import { getFullnodeUrl, SuiClient } from '@mysten/sui/client'
-import { Transaction } from '@mysten/sui/transactions'
+import { getFullnodeUrl, SuiClient } from "@mysten/sui.js/client";
+import { TransactionBlock } from '@mysten/sui.js/transactions';
+import {
+    fromB64,
+} from "@mysten/sui.js/utils";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+
+
+
 
 const PBT_PACKAGE_ID = '0x93d0628b3b4a9b5b9ebde175e8c51fa7b6296e5f9aaf5d9645eca3b17a29a111'
 export const mySuiClient = new SuiClient({ url: getFullnodeUrl('testnet') })
+
 
 
 export const getUserData = ()=> {
@@ -26,30 +32,25 @@ export const VerifySignature = async (signature_final:  Uint8Array, pkey_final: 
         userKeypair,
     }  = getUserData()
 
-    const tx = new Transaction()
+    const tx = new TransactionBlock();
     tx.setGasBudget(2000000)
-
     tx.moveCall({
         target: `${PBT_PACKAGE_ID}::signature::verify_signature_v3`,
         arguments: [
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
             tx.pure(Array.from(signature_final)),
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
             tx.pure(Array.from(pkey_final)),
         ],
     })
-    const response = await mySuiClient.signAndExecuteTransaction({
+    const response = await mySuiClient.signAndExecuteTransactionBlock({
         signer: userKeypair,
-        transaction: tx,
-        requestType: 'WaitForLocalExecution',
+        transactionBlock: tx,
+        requestType: "WaitForLocalExecution",
         options: {
             showEffects: true,
             showEvents: true,
             showObjectChanges: true,
         },
-    })
+    });
 
     console.log(response)
 }
